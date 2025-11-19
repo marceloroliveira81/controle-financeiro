46b0a38ca9b93780549839351cb04bdf3ebf6ea9
[dyad] <git@dyad.sh>
2025-11-19 10:30:47 -0500

    [dyad] Permitindo lançamentos com datas futuras - wrote 1 file(s)

--- src/components/TransactionForm.tsx
+++ src/components/TransactionForm.tsx
@@ -161,7 +161,7 @@ export const TransactionForm = ({ setOpen, transaction }: TransactionFormProps) 
                     selected={field.value}
                     onSelect={field.onChange}
                     disabled={(date) =>
-                      date > new Date() || date < new Date('1900-01-01')
+                      date < new Date('1900-01-01')
                     }
                     initialFocus
                     locale={ptBR}
